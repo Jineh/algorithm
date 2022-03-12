26c3d318001f3d7d5549b287ae0ec2a1a7629cfc
Jineh <lst5518@naver.com>
2022-03-12 19:48:01 +0900

    3-11

---
+++ python algorithm/3/3-11.py
@@ -0,0 +1,14 @@
+import sys
+board = [list(map(int, sys.stdin().readline().split())) for _ in range(7)]
+cnt =0
+for i in range(3):
+    for j in range(7):  # 행 검사
+        tmp=board[j][i:i+5]
+        if tmp==tmp[::-1]:
+            cnt+=1
+        for k in range(2): # 5개 비교니까 2번만 회문 비교하면 됨
+            if board[i+k][j]!=board[i+5-k-1][j]:
+                break
+        else:
+            cnt+=1
+print(cnt)

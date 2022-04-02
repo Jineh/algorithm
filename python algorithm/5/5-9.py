748f0e41e56acd8a121e478d31eaab3cde3be590
Jineh <lst5518@naver.com>
2022-04-02 22:14:33 +0900

    5-9

--- python algorithm/5/5-9.py
+++ python algorithm/5/5-9.py
@@ -2,22 +2,39 @@ import sys
 
 s1=sys.stdin.readline()
 s2=sys.stdin.readline()
-d = dict()
+d1 = dict()
+d2 = dict()
 
 for x in s1:
-    if x in d:
-        d[x]+=1
-    else:
-        d[x]=0
+    d1[x]=d1.get(x, 0)+1    # 처음 값 설정 없으면 0 반환
 for x in s2:
-    if x in d:
-        if d[x]==0:
+    d2[x]=d2.get(x, 0)+1
+for i in d1.keys():
+    if i in d1.keys():      # key 값 있는지 확인ㄴ
+        if d1[i]!=d2[i]:    # 개수 비교
             print("NO")
             break
-        else:
-            d[x]-=1
     else:
         print("NO")
+        break
 else:
     print("YES")
+
+
+# for x in s1:
+#     if x in d:
+#         d[x]+=1
+#     else:
+#         d[x]=0
+# for x in s2:
+#     if x in d:
+#         if d[x]==0:
+#             print("NO")
+#             break
+#         else:
+#             d[x]-=1
+#     else:
+#         print("NO")
+# else:
+#     print("YES")
 

fcc3eea3304b9e2da883fb485871be223c7b2f7a
Jineh <lst5518@naver.com>
2022-04-07 22:04:30 +0900

    6-2

--- python algorithm/5/5-10.py
+++ python algorithm/5/5-10.py
@@ -1,12 +1,12 @@
 import sys
 import heapq as hq
-a=[]
+a=[]        # 리스트에 트리 형태로 저장 
 while True:
     n= int(sys.stdin.readline())
     if n==-1:
         break
     if n==0:
-        if len(a)==0:
+        if len(a)==0:   # 리스트 빈 값인지 확인
             print(-1)
         else:
             print(hq.heappop(a))
---
+++ python algorithm/6/6-2.py
@@ -0,0 +1,12 @@
+import sys
+
+def DFS(v):
+    if v>7:
+        return
+    else:
+        print(v, end=' ')   # 전위순회
+        DFS(v*2)
+        DFS(v*2+1)
+
+if __name__=="__main__":
+    DFS(1)

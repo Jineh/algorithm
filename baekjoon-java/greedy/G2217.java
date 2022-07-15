0fbbaa7227fe7387275287b28225e858858389f4
Jineh <lst5518@naver.com>
2022-07-15 17:50:15 +0900

    greedy

---
+++ baekjoon-java/greedy/G2217.java
@@ -0,0 +1,35 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class G2217 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n  = Integer.parseInt(br.readLine());
+        int[] rope = new int[n];
+        StringTokenizer st;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            rope[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(rope);
+        int max = rope[n-1];
+
+        for(int i =0; i<n; i++){
+            max = Math.max(max, rope[i]*(n-i));
+        }
+
+        System.out.println(max);
+
+    }
+
+
+}
